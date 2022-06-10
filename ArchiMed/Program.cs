3d0101cbaf01d1bc19d1c644b5fb1d2926e1ae32
Firas GACHA <33499639+firasgacha@users.noreply.github.com>
2022-06-10 12:37:34 +0100

    crud patient done

--- ArchiMed/Program.cs
+++ ArchiMed/Program.cs
@@ -39,55 +39,55 @@ app.MapControllers();
 
 app.MapGet("/", () => "Hello World!");
 
-app.MapPost("/patient/", async(Patient n, PatientDb db)=> {
-    db.Patients.Add(n);
-    await db.SaveChangesAsync();
-
-    return Results.Created($"/patient/{n.PatientId}", n);
-});
-
-app.MapGet("/patient/{id:int}", async(int id, PatientDb db)=> 
-{
-    return await db.Patients.FindAsync(id)
-        is Patient n
-        ? Results.Ok(n)
-        : Results.NotFound();
-});
-
-app.MapGet("/patient", async (PatientDb db) => await db.Patients.ToListAsync());
-
-app.MapPut("/notes/{id:int}", async(int id, Patient n, PatientDb db)=>
-{
-    if (n.PatientId != id)
-    {
-        return Results.BadRequest();
-    }
-
-    var patient = await db.Patients.FindAsync(id);
-    
-    if (patient is null) return Results.NotFound();
-
-    //found, so update with incoming note n.
-    patient.nom = n.nom;
-    patient.prenom = n.prenom;
-    
-    await db.SaveChangesAsync();
-    return Results.Ok(patient);
-});
-
-app.MapDelete("/notes/{id:int}", async(int id, PatientDb db)=>{
-
-    var patient = await db.Patients.FindAsync(id);
-    if (patient is not null){
-        db.Patients.Remove(patient);
-        await db.SaveChangesAsync();
-    }
-    return Results.NoContent();
-});
+// app.MapPost("/patient/", async(Patient n, PatientDb db)=> {
+//     db.Patients.Add(n);
+//     await db.SaveChangesAsync();
+//
+//     return Results.Created($"/patient/{n.PatientId}", n);
+// });
+//
+// app.MapGet("/patient/{id:int}", async(int id, PatientDb db)=> 
+// {
+//     return await db.Patients.FindAsync(id)
+//         is Patient n
+//         ? Results.Ok(n)
+//         : Results.NotFound();
+// });
+//
+// app.MapGet("/patient", async (PatientDb db) => await db.Patients.ToListAsync());
+//
+// app.MapPut("/notes/{id:int}", async(int id, Patient n, PatientDb db)=>
+// {
+//     if (n.PatientId != id)
+//     {
+//         return Results.BadRequest();
+//     }
+//
+//     var patient = await db.Patients.FindAsync(id);
+//     
+//     if (patient is null) return Results.NotFound();
+//
+//     //found, so update with incoming note n.
+//     patient.nom = n.nom;
+//     patient.prenom = n.prenom;
+//     
+//     await db.SaveChangesAsync();
+//     return Results.Ok(patient);
+// });
+//
+// app.MapDelete("/notes/{id:int}", async(int id, PatientDb db)=>{
+//
+//     var patient = await db.Patients.FindAsync(id);
+//     if (patient is not null){
+//         db.Patients.Remove(patient);
+//         await db.SaveChangesAsync();
+//     }
+//     return Results.NoContent();
+// });
 
 app.Run();
 
-class PatientDb: DbContext {
+public class PatientDb: DbContext {
     public PatientDb(DbContextOptions<PatientDb> options): base(options) {
 
     }
