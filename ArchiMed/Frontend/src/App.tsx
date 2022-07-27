5f4a622c2512f988349917da6fecd8c044f01f98
Firas GACHA <33499639+firasgacha@users.noreply.github.com>
2022-07-27 13:11:27 +0100

    (F) Fix the home page layout

--- ArchiMed/Frontend/src/App.tsx
+++ ArchiMed/Frontend/src/App.tsx
@@ -1,23 +1,26 @@
 import "./App.css";
 import Home from "./pages/home";
-import {Routes, Route, Navigate} from "react-router-dom";
-import Profile from "./pages/profile";
-import Login from "./pages/Login";
-import NotFound from "./pages/NotFound";
+import {Routes, Route, Navigate, BrowserRouter} from "react-router-dom";
+import Login from "pages/Login";
+import NotFound from "pages/NotFound";
+import React from "react";
+import Profile from "./pages/home/profile";
 
 function App() {
   return (
     <>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/profile" element={<Profile />} />
+        <Route element={<Home />} >
+              <Route path="profile" element={<Profile />} />
+        </Route>
         <Route path="/login" element={<Login />} />
-        <Route path="/NotFound" element={<NotFound />} />
-        <Route path="/NotFound" element={<NotFound />} />
-          <Route
-              path="*"
-              element={<Navigate to="/NotFound" replace />}
-          />
+        {/*<Route path="/signup" element={<Signup />} />*/}
+        {/*<Route path="/about" element={<About />} />*/}
+        {/*<Route path="/NotFound" element={<NotFound />} />*/}
+        {/*  <Route*/}
+        {/*      path="*"*/}
+        {/*      element={<Navigate to="/NotFound" replace />}*/}
+        {/*  />*/}
       </Routes>
     </>
   );
--- ArchiMed/Frontend/src/pages/home/profile.tsx
+++ ArchiMed/Frontend/src/pages/home/profile.tsx
@@ -1,12 +1,10 @@
 import { useState } from "react";
-import Navbar from "../../components/Navbar";
 
 export default function Profile() {
   const [count, setCount] = useState(0);
 
   return (
     <>
-      <Navbar />
         <div>Profile</div>
     </>
   );
