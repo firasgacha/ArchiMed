2ff231a71917fb72f3540d4e3953eea3b9dc9bed
Firas GACHA <33499639+firasgacha@users.noreply.github.com>
2022-08-09 18:59:01 +0100

    F: update index and add 2 pages

--- ArchiMed/Frontend/src/pages/home/index.tsx
+++ ArchiMed/Frontend/src/pages/home/index.tsx
@@ -222,11 +222,13 @@ export default function Navbar() {
                         </div>
                       </div>
                       <div className="h-full w-20 flex items-center justify-center border-r mr-4 cursor-pointer text-gray-600">
-                        <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-messages" width={28} height={28} viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round">
-                          <path stroke="none" d="M0 0h24v24H0z" />
-                          <path d="M21 14l-3 -3h-7a1 1 0 0 1 -1 -1v-6a1 1 0 0 1 1 -1h9a1 1 0 0 1 1 1v10" />
-                          <path d="M14 15v2a1 1 0 0 1 -1 1h-7l-3 3v-10a1 1 0 0 1 1 -1h2" />
-                        </svg>
+                        <Link to={'/contact'}>
+                          <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-messages" width={28} height={28} viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round">
+                            <path stroke="none" d="M0 0h24v24H0z" />
+                            <path d="M21 14l-3 -3h-7a1 1 0 0 1 -1 -1v-6a1 1 0 0 1 1 -1h9a1 1 0 0 1 1 1v10" />
+                            <path d="M14 15v2a1 1 0 0 1 -1 1h-7l-3 3v-10a1 1 0 0 1 1 -1h2" />
+                          </svg>
+                        </Link>
                       </div>
                       <div className="flex items-center relative cursor-pointer" onClick={() => setProfile(!profile)}>
                         <div className="rounded-full">
